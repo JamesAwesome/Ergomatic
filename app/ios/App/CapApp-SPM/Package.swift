93e3f2cf2b942a14e0e75b3ea4cc6cb25f980935
James Awesome <james@morelli.nyc>
2026-09-03 20:07:12 -0400

    fix: serialize NFC native session generations

--- app/ios/App/CapApp-SPM/Package.swift
+++ app/ios/App/CapApp-SPM/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(name: "CapacitorCommunityKeepAwake", path: "../../../node_modules/.pnpm/@capacitor-community+keep-awake@8.0.1_@capacitor+core@8.5.0/node_modules/@capacitor-community/keep-awake"),
         .package(name: "CapacitorApp", path: "../../../node_modules/.pnpm/@capacitor+app@8.1.1_@capacitor+core@8.5.0/node_modules/@capacitor/app"),
         .package(name: "CapacitorBrowser", path: "../../../node_modules/.pnpm/@capacitor+browser@8.0.4_@capacitor+core@8.5.0/node_modules/@capacitor/browser"),
-        .package(name: "CapgoCapacitorNfc", path: "../../../node_modules/.pnpm/@capgo+capacitor-nfc@8.2.5_patch_hash=ebd4b1f4af6c0f1a8515b4b8b8ca23f367f021317440b4371_849806535db05dbe95564b03539a00ef/node_modules/@capgo/capacitor-nfc"),
+        .package(name: "CapgoCapacitorNfc", path: "../../../node_modules/.pnpm/@capgo+capacitor-nfc@8.2.5_patch_hash=b4b160fc338c9d4dab80eb850a84d9205f4ad8a4fb1ed8124_5d7c5389d533c058a5b2fe1d4d66c1a7/node_modules/@capgo/capacitor-nfc"),
         .package(name: "CapgoCapacitorSocialLogin", path: "../../../node_modules/.pnpm/@capgo+capacitor-social-login@8.4.4_@capacitor+core@8.5.0/node_modules/@capgo/capacitor-social-login")
     ],
     targets: [
